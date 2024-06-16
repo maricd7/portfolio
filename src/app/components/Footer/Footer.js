5d1d9b79bb7588e9d32d2845cf4cad0a765c0e59
unknown <dejanmaric259@gmail.com>
2024-06-16 23:33:46 +0200

    footer update

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,21 +1,32 @@
-import React from 'react'
-
+"use client";
+import Image from "next/image";
+import React from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
 function Footer() {
   return (
     <footer className=" text-white py-8 w-full mt-10">
-    <div className="container mx-auto text-center">
-      <p className='text-white'>
-        &copy; {new Date().getFullYear()} Dejan Maric. All rights reserved.
-      </p>
-      <p className="mt-2 text-white ">
-        Made with{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-      </p>
-    </div>
-  </footer>
-  )
+      <div className="container mx-auto text-center">
+        <p className="text-white">
+          &copy; {new Date().getFullYear()} Dejan Maric. All rights reserved.
+        </p>
+        <p className="mt-2 text-white flex items-center justify-center gap-8">
+          Made with{" "}
+          <Image
+            src="/tech-icons/nextjs.svg"
+            alt="Next Js"
+            height={32}
+            width={64}
+          />
+          <Icon
+            icon="devicon-plain:tailwindcss-wordmark"
+            width="128"
+            height="128"
+            style={{ color: "#fff" }}
+          />
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
